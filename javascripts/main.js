dd69845e5c719646f0a2dc808c5bd0ca9d7afdcf
sharon smith <sharon.e.smith2@gmail.com>
2017-05-12 12:27:02 -0500

    navbar done

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,2 +1,6 @@
 
-console.log("writing to console");
+var app = angular.module("laurenFoods", []);
+
+app.controller("NavCtrl", ($scope) => {
+	$scope.navItems = [{name: "Fave Foods"}, {name: "Lauren's Fave Animal"}];
+});
